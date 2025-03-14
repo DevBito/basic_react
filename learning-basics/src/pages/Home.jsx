d579197ce13cc378f73a8bed8163a34dd546f82d
Gabriel DevBito <gabrielgc765@gmail.com>
2025-03-14 13:42:16 -0300

    Hahah muita diversão:
    > Aprendi a modularizar
    > Relembrei alguns comandos de mkdir e cd
    > criar páginas e importar corretamente no App.js
    > Sempre se atentar aos caracteres minusculos e maiúsculo
    > package.json é quase igual um requirements

---
+++ learning-basics/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/Home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+        <h1>Aqui é a tela principal!</h1>
+        <p>Seja bem vindo amiguinho teste os botões abaixo:</p>
+        </div>
+    );
+};
+
+export default Home;
